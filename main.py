3d4041b2351dc0f69aa99755abc502ed305ed2ea
Fendly98 <f.belhomme.jouvet@gmail.com>
2022-01-08 23:37:44 +0100

    update size

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ clock = pygame.time.Clock()
 snake_block = 10
 snake_speed = 10
  
-font_style = pygame.font.SysFont("bahnschrift", 8)
+font_style = pygame.font.SysFont("bahnschrift", 30)
 score_font = pygame.font.SysFont("comicsansms", 15)
  
 def Your_score(score):
